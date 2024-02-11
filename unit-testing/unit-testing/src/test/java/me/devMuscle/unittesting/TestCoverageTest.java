bda2517aac18373a4eda5fe089ac925d6d87d356
devMuscle <hongsbien@naver.com>
2024-02-11 16:13:54 +0900

    커버리지 지표í가 모든 결과를 ê검증한다고 보증할 수 없는 경우 예시 추가

--- unit-testing/unit-testing/src/test/java/me/devMuscle/unittesting/TestCoverageTest.java
+++ unit-testing/unit-testing/src/test/java/me/devMuscle/unittesting/TestCoverageTest.java
@@ -6,15 +6,35 @@ import org.junit.jupiter.api.Test;
 public class TestCoverageTest {
 
     @Test
-    public void isStringLong1Test() {
+    public void isStringLong1_테스트_커버리지_80() {
         boolean result = isStringLong1("abc");
         Assertions.assertEquals(false, result);
     }
 
     @Test
-    public void isStringLong2Test() {
+    public void isStringLong2_테스트_커버리지_100() {
         boolean result = isStringLong2("abc");
         Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void 마지막_결과만을_기록하는_경우() {
+        boolean result = isStringLong("abc");
+        Assertions.assertEquals(false, result); // 두 번째 결과만 검증
+    }
+
+    @Test
+    public void 검증이_없는_테스트는_언제나_통과한다() {
+        boolean result1 = isStringLong("abc");
+        boolean result2 = isStringLong("abcdef");
+    }
+
+    public static boolean wasLastStringLong;
+
+    public static boolean isStringLong(String input) {
+        boolean result = input.length() > 5;
+        wasLastStringLong = result; //첫 번째 결과
+        return result; // 두 번째 결과
     }
 
     public static boolean isStringLong1(String input) {
