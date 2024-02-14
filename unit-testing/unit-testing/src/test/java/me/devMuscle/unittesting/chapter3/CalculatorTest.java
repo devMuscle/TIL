fd2ae39a56fda75b1b7c761c4ac611719107b078
devMuscle <hongsbien@naver.com>
2024-02-14 23:03:53 +0900

    AAA 패턴 예시 코드 자작성

---
+++ unit-testing/unit-testing/src/test/java/me/devMuscle/unittesting/chapter3/CalculatorTest.java
@@ -0,0 +1,22 @@
+package me.devMuscle.unittesting.chapter3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    public void sum_of_two_numbers() {
+        // 준비
+        double first = 10;
+        double second = 20;
+        Calculator calculator = new Calculator();
+
+        // 실행
+        double result = calculator.sum(first, second);
+
+        // 검증
+        assertEquals(30, result);
+    }
+}
