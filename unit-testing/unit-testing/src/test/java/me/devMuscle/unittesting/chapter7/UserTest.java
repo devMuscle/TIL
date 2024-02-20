f9ddfda1950b62416069f516aafd83bc912d6073
devMuscle <hongsbien@naver.com>
2024-02-20 23:19:00 +0900

    도메인 계층과 유틸리티 코드 테스트하기

---
+++ unit-testing/unit-testing/src/test/java/me/devMuscle/unittesting/chapter7/UserTest.java
@@ -0,0 +1,33 @@
+package me.devMuscle.unittesting.chapter7;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    @Test
+    public void changing_email_from_non_corporate_to_corporate() {
+        Company company = new Company("mycorp.com", 1);
+        User sut = new User(1, "user@gmail.com", UserType.CUSTOMER);
+
+        sut.changeEmail("new@mycorp.com", company);
+
+        assertEquals(2, company.getNumberOfEmployees());
+        assertEquals("new@mycorp.com", sut.email);
+        assertEquals(UserType.EMPLOYEE, sut.type);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"mycorp.com, email@mycorp.com, true", "mycorp.com, email@gamil.com, false"})
+    public void differentiates_a_corporate_email_from_non_corporate (String domain, String email, boolean expectedResult) {
+
+        Company sut = new Company(domain, 0);
+
+        boolean isEmailCorporate = sut.isEmailCorporate(email);
+
+        assertEquals(expectedResult, isEmailCorporate);
+    }
+}
