27e0b43541692d15fdd63e1d44138c5e06d54a64
devMuscle <hongsbien@naver.com>
2024-02-20 23:46:45 +0900

    모든 의사 결정을 제거한 컨트롤러

--- unit-testing/unit-testing/src/main/java/me/devMuscle/unittesting/chapter7/UserController.java
+++ unit-testing/unit-testing/src/main/java/me/devMuscle/unittesting/chapter7/UserController.java
@@ -6,17 +6,25 @@ public class UserController {
     private final Database database = new Database();
     private final MessageBus messageBus = new MessageBus();
 
-    public void changeEmail(int userId, String newEmail) {
+    public String changeEmail(int userId, String newEmail) {
+        // 데이터 준비
         Object[] userData = database.getUserById(userId);
         User user = UserFactory.create(userData);
 
         Object[] companyData = database.getCompany();
         Company company = CompanyFactory.create(companyData);
 
-        user.changeEmail(newEmail, company);
+        // 의사 결정
+        String error = user.changeEmail(newEmail, company);
+        if(error != null) {
+            return error;
+        }
 
+        // 결정에 따라 실행하기
         database.saveCompany(company);
         database.saveUser(user);
         messageBus.sendEmailChangedMessage(userId, newEmail);
+
+        return "OK";
     }
 }
