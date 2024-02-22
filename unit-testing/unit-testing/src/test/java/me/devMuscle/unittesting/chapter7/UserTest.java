dd9f905cdb826251cc3141942a8d7bbb2a4a93ea
devMuscle <hongsbien@naver.com>
2024-02-22 23:33:29 +0900

    통합 테스트 작성

--- unit-testing/unit-testing/src/test/java/me/devMuscle/unittesting/chapter7/UserTest.java
+++ unit-testing/unit-testing/src/test/java/me/devMuscle/unittesting/chapter7/UserTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class UserTest {
 
@@ -33,7 +35,7 @@ public class UserTest {
     }
 
     @Test
-    public void changing_email_from_corporate_to_non_corporate() {
+    public void changing_email_from_corporate_to_non_corporate_unit_test() {
         Company company = new Company("mycorp.com", 1);
         User sut = new User(1, "user@mycorp.com", UserType.EMPLOYEE, false);
 
@@ -43,5 +45,38 @@ public class UserTest {
         Assertions.assertThat(sut.getEmail()).isEqualTo("new@gmail.com");
         Assertions.assertThat(sut.getType()).isEqualTo(UserType.CUSTOMER);
         Assertions.assertThat(sut.getEmailChangedEvents()).contains(new EmailChangedEvent(1, "new@gmail.com"));
+    }
+
+    @Test
+    public void changing_email_from_corporate_to_non_corporate_integration_test() {
+        //준비
+        Database db = new Database();
+        //데이터베이스에 사용자와 회사 생성
+        User user = UserFactory.createUser("user@mycorp.com", UserType.EMPLOYEE, db);
+        CompanyFactory.createCompany("mycorp.com", 1, db);
+
+        //메시지 버스 목 설정
+        MessageBus messageBusMock = mock(MessageBus.class);
+        UserController sut = new UserController(db, messageBusMock);
+
+        //실행
+        String result = sut.changeEmail(user.getUserId(),"new@gmail.com");
+
+        //검증
+        assertEquals("OK", result);
+
+        //사용자 상태 검증
+        Object[] userData = db.getUserById(user.getUserId());
+        User userFromDb = UserFactory.create(userData);
+        assertEquals("new@gmail.com", userFromDb.getEmail());
+        assertEquals(UserType.CUSTOMER, userFromDb.getType());
+
+        //회사 상태 검증
+        Object[] companyData = db.getCompany();
+        Company companyFromDb = CompanyFactory.create(companyData);
+        assertEquals(0, companyFromDb.getNumberOfEmployees());
+
+        //목 상호 작용 확인
+        verify(messageBusMock, times(1)).sendEmailChangedMessage(user.getUserId(), "new@gmail.com");
     }
 }
