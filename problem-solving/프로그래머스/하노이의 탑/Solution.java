57b2dcb27d77d4ac5845006acae6868296ce8e56
devMuscle <hongsbien@naver.com>
2024-03-07 21:12:28 +0900

    코드 최적화

--- problem-solving/프로그래머스/하노이의 탑/Solution.java
+++ problem-solving/프로그래머스/하노이의 탑/Solution.java
@@ -1,20 +1,22 @@
 import java.util.*;
 
 class Solution {
-    private List<int[]> hanoi(int n, int from, int to) {
-        if(n == 1) return List.of(new int[] {from, to});
+    private void hanoi(int n, int from, int to, List<int[]> process) {
+        if(n == 1) {
+            process.add(new int[] {from, to});
+            return;
+        }
 
         int empty = 6 - from - to;
 
-        List<int[]> result = new ArrayList<>();
-        result.addAll(hanoi(n-1, from, empty));
-        result.addAll(hanoi(1, from, to));
-        result.addAll(hanoi(n-1, empty, to));
-
-        return result;
+        hanoi(n-1, from, empty, process);
+        hanoi(1, from, to, process);
+        hanoi(n-1, empty, to, process);
     }
 
     public int[][] solution(int n) {
-        return hanoi(n,1,3).toArray(new int[0][]);
+        List<int[]> process = new ArrayList<>();
+        hanoi(n, 1, 3, process);
+        return process.toArray(new int[0][]);
     }
 }
